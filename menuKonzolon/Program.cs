49827b59f84a993fc9004dbb434e49ad59d3f6c9
FMáté <fejesm@kkszki.hu>
2025-09-11 13:02:37 +0200

    Menüszerkezet kialakítása

--- menuKonzolon/Program.cs
+++ menuKonzolon/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,39 @@ namespace menuKonzolon
     {
         static void Main(string[] args)
         {
+            string nev = "";
+            bool ffi = true;
+            string szulEV; 
+            string valasz = "";
+            do
+            {
+                //Console.Beep();
+                Console.Clear();
+                Console.Title = "Személyes adatok kezelése";
+                Console.WriteLine("6 - Személyes adatok bevitele");
+                Console.WriteLine("7 - Személyes adatok kiírása");
+                Console.WriteLine("67 - Kilépés\n");
+                Console.WriteLine("A megfelelő menüpont számával választhat");
+                valasz = Console.ReadLine();
+                //if-fel szépen végigmehetnénk
+                if (valasz == "6")
+                {
+                    Console.WriteLine("SIkeresen rögzítettük az adatokat. Enterre tovább...");
+                    Console.ReadLine();
+                }
+                else if (valasz == "7")
+                {
+
+                }
+                else if (valasz == "67")
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("Nem létező menüpontm, kérem válasszon másikat");
+                }
+            } while (valasz != "3");
         }
     }
 }
